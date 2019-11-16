7e3449c63199fe49f062f88c7b55216067562e70
monicanq <monicanq@gmail.com>
2019-11-16 21:41:25 +0100

    Updates

---
+++ precache-manifest.11833ac4e1dc699bb2a90d9e7d40732f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a87e435b677beaf2f2444c5baf52493",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "da3eecce20f6813e1f31",
+    "url": "/random-quote/static/css/main.c6a3ccc7.chunk.css"
+  },
+  {
+    "revision": "d65af21f42fd6b936184",
+    "url": "/random-quote/static/js/2.01541a37.chunk.js"
+  },
+  {
+    "revision": "da3eecce20f6813e1f31",
+    "url": "/random-quote/static/js/main.a6941bcf.chunk.js"
+  },
+  {
+    "revision": "b175e3523d8e889c827e",
+    "url": "/random-quote/static/js/runtime-main.3705c828.js"
+  }
+]);
